2ec14e6dc1eab9308782be18392aa139d6da7b7e
CantarinoG <guilhermecantarino1709@gmail.com>
2024-01-31 19:05:13 -0300

    Add popup menu item

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shop/components/product_grid.dart';
 
+enum FilterOptions { Favorite, All }
+
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({super.key});
 
@@ -11,6 +13,23 @@ class ProductsOverviewPage extends StatelessWidget {
         title: Text(
           "Minha Loja",
         ),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text("Somente Favoritos"),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text("Todos os Produtos"),
+                value: FilterOptions.All,
+              )
+            ],
+            onSelected: (FilterOptions selectedValue) {
+              print(selectedValue);
+            },
+          ),
+        ],
       ),
       body: ProductGrid(),
     );
