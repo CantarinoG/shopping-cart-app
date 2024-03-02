6c9d667d3c9dbe831325f5dc2df9ebadd1ee6d07
CantarinoG <guilhermecantarino1709@gmail.com>
2024-03-02 19:21:16 -0300

    Add dismiss confirm when deleting an item

--- lib/components/product_grid_item.dart
+++ lib/components/product_grid_item.dart
@@ -54,6 +54,7 @@ class ProductGridItem extends StatelessWidget {
           ),
           trailing: IconButton(
             onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                 content: Text("Produto adicionado com sucesso!"),
                 duration: Duration(seconds: 2),
