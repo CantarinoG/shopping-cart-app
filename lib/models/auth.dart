ae12d56e25a9580da4a36eda6bb843c946cd4878
CantarinoG <guilhermecantarino1709@gmail.com>
2024-03-28 13:17:50 -0300

    feat: Implement user login logic

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -7,9 +7,13 @@ import 'package:shop/utils/secrets.dart';
 class Auth with ChangeNotifier {
   static const _url = Secrets.API_SIGNUP_URL;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, bool isLogin) async {
+    final url = isLogin ? Secrets.API_LOGIN_URL : Secrets.API_SIGNUP_URL;
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(
+        url,
+      ),
       body: jsonEncode(
         {
           "email": email,
@@ -18,5 +22,13 @@ class Auth with ChangeNotifier {
         },
       ),
     );
+  }
+
+  Future<void> signup(String email, String password) async {
+    _authenticate(email, password, false);
+  }
+
+  Future<void> login(String email, String password) async {
+    _authenticate(email, password, true);
   }
 }
