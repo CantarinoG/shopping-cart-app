82d771145db7c86d4cbd435c9ff9af0a11e87d7c
CantarinoG <guilhermecantarino1709@gmail.com>
2024-03-29 20:42:13 -0300

    ui: Add transition animation for container size in AuthPage
    In AuthPage, the container needs to be bigger if user is creating an account and smaller if user is logging in. The size transition in the container is now animated.

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -15,7 +15,8 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   AuthMode _authMode = AuthMode.Login;
 
   final _passwordController = TextEditingController();
@@ -29,16 +30,49 @@ class _AuthFormState extends State<AuthForm> {
     "password": "",
   };
 
+  AnimationController? _controller;
+  Animation<Size>? _heightAnimation;
+
   bool _isLogin() => _authMode == AuthMode.Login;
   bool _isSignUp() => _authMode == AuthMode.Signup;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 300,
+      ),
+    );
+
+    _heightAnimation = Tween(
+      begin: Size(double.infinity, 310),
+      end: Size(double.infinity, 400),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.linear,
+      ),
+    );
+    _heightAnimation?.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
 
   void _switchAuthMode() {
     setState(
       () {
         if (_isLogin()) {
           _authMode = AuthMode.Signup;
+          _controller?.forward();
         } else {
           _authMode = AuthMode.Login;
+          _controller?.reverse();
         }
       },
     );
@@ -108,7 +142,7 @@ class _AuthFormState extends State<AuthForm> {
       ),
       child: Container(
         padding: EdgeInsets.all(16),
-        height: _isLogin() ? 310 : 400,
+        height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
         width: deviceSize.width * 0.75,
         child: Form(
           key: _formKey,
